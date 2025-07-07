a8a8198cc8a646b06219dff7b80a11998b46aa85
Ozan YAPRAK <ozanerhanyaprak@icloud.com>
2025-07-07 11:51:14 +0300

    #

---
+++ RelationsProject/APIs/ApiExtensions.cs
@@ -0,0 +1,92 @@
+﻿using RelationsProject.Abstracts;
+using RelationsProject.DTOs;
+using RelationsProject.Entities;
+using RelationsProject.Exceptions.BookExceptions;
+using System.ComponentModel.DataAnnotations;
+
+namespace RelationsProject.APIs
+{
+    public static class ApiExtensions
+    {
+        public static void GetAllBooks(this WebApplication app)
+        {
+            app.MapGet("/api/books", (IBookService bookService) =>
+            {
+                return bookService.Count > 0
+                    ? Results.Ok(bookService.GetBooks())
+                    : throw new BookNotFoundException(0);
+            })
+            .Produces<List<BookDTO>>(StatusCodes.Status200OK)
+            .Produces(StatusCodes.Status204NoContent)
+            .WithTags("CRUD", "GETs");
+        }
+
+        public static void GetBookById(this WebApplication app)
+        {
+            app.MapGet("/api/books/{id}", (int id, IBookService bookService) =>
+            {
+                var book = bookService.GetBookById(id); // Kitap listesinden ID'ye göre kitap arar.
+                return Results.Ok(book); // Eğer kitap bulunursa, kitap bilgilerini döndürür. // Eğer kitap bulunamazsa, hata fırlatır.
+
+                //return Results.Ok(Book.ListWithId(id)); // Book sınıfındaki ListWithId metodunu kullanarak ID'ye göre kitap döndürür.
+            })
+                .Produces<Book>(StatusCodes.Status200OK) // Produces, Swagger'da bu endpoint'in başarılı durum kodunu ve başarılı durum mesajını gösterir.
+                .Produces<ErrorDetails>(StatusCodes.Status404NotFound) // Eğer kitap bulunamazsa, 404 Not Found durum kodunu döndürür.
+                .WithTags("GETs"); // WithTags, Swagger'da bu endpoint'in hangi gruba ait olduğunu gösterir. // Bu endpoint'i "GETs" grubuna ekler.
+
+        }
+
+        public static void PostBook(this WebApplication app)
+        {
+            app.MapPost("/api/books", (BookDtoForInsertion InsertBook, IBookService bookService) =>
+            {
+                var book = bookService.CreateBook(InsertBook);
+                return Results.Created($"/api/books/{book.Id}", InsertBook); // 201
+            })
+                .Produces<Book>(StatusCodes.Status201Created) // Produces, Swagger'da bu endpoint'in başarılı durum kodunu ve başarılı durum mesajını gösterir.
+                .Produces<List<ValidationResult>>(StatusCodes.Status422UnprocessableEntity) // Eğer kitap doğrulama hatası varsa, 422 Unprocessable Entity durum kodunu döndürür.
+                .WithTags("CRUD"); // WithTags, Swagger'da bu endpoint'in hangi gruba ait olduğunu gösterir. // Bu endpoint'i "CRUD" grubuna ekler.
+        }
+
+        public static void PutBook(this WebApplication app)
+        {
+            app.MapPut("/api/books/{id}", (int id, BookDtoForUpdate updateBook, IBookService bookService) =>
+            {
+                Book book = bookService.UpdateBook(id, updateBook);
+                return Results.Ok(book); // 200
+            })
+                .Produces<Book>(StatusCodes.Status200OK)
+                .Produces<ErrorDetails>(StatusCodes.Status404NotFound)
+                .Produces<ErrorDetails>(StatusCodes.Status400BadRequest)
+                .Produces<ErrorDetails>(StatusCodes.Status422UnprocessableEntity)
+                .WithTags("CRUD"); // WithTags, Swagger'da bu endpoint'in hangi gruba ait olduğunu gösterir. // Bu endpoint'i "CRUD" grubuna ekler.
+        }
+
+        public static void DeleteBook(this WebApplication app)
+        {
+            app.MapDelete("/api/books/{id}", (int id, IBookService bookService) =>
+            {
+                bookService.DeleteBook(id); // Kitabı kitap listesinden siler.
+                return Results.NoContent(); // 204
+            })
+                .Produces(StatusCodes.Status204NoContent)
+                .Produces<ErrorDetails>(StatusCodes.Status404NotFound)
+                .Produces<ErrorDetails>(StatusCodes.Status400BadRequest)
+                .WithTags("CRUD"); // WithTags, Swagger'da bu endpoint'in hangi gruba ait olduğunu gösterir. // Bu endpoint'i "CRUD" grubuna ekler.
+        }
+
+        public static void SearchBooks(this WebApplication app)
+        {
+            app.MapGet("/api/books/search", (string? title, IBookService bookService) =>
+            {
+                var books = string.IsNullOrEmpty(title) ? bookService.GetBooks() : bookService.GetBooks().Where(x => x.Title is not null && x.Title.Contains(title, StringComparison.OrdinalIgnoreCase)).ToList(); // Eğer title boş ise, tüm kitapları döndürür. // Eğer title dolu ise, kitap listesinden title'a göre arama yapar. // StringComparison.OrdinalIgnoreCase, büyük/küçük harf duyarsız arama yapar.
+
+                return books.Any() ? Results.Ok(books) : Results.NotFound(); // Eğer kitap listesi boş değilse, kitap listesini döndürür. // Eğer kitap listesi boşsa, 404 Not Found durum kodunu döndürür.
+            })
+                .Produces<List<Book>>(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status204NoContent)
+                .Produces<ErrorDetails>(StatusCodes.Status400BadRequest)
+                .WithTags("GETs");
+        }
+    }
+}
--- RelationsProject/Program.cs
+++ RelationsProject/Program.cs
@@ -1,4 +1,5 @@
 using RelationsProject.Abstracts;
+using RelationsProject.APIs;
 using RelationsProject.Configuration;
 using RelationsProject.DTOs;
 using RelationsProject.Entities;
@@ -28,78 +29,24 @@ if (app.Environment.IsDevelopment()) // Uygulama geliþtirme ortamýnda ise Swagge
     app.UseSwaggerUI();
 }
 
-// GET
+// GET ERROR
 app.MapGet("/api/error", () =>
 {
     throw new Exception("An error has been occured.");
 }).Produces<ErrorDetails>(StatusCodes.Status500InternalServerError) // Produces, Swagger'da bu endpoint'in hata kodunu ve hata mesajýný gösterir.
 .ExcludeFromDescription(); // ExcludeFromDescription, Swagger'da bu endpoint'i gizler. // Bu endpoint'i Swagger'da görmek istemiyorsanýz kullanabilirsiniz.
 
-// GET
-app.MapGet("/api/books", (IBookService bookService) =>
-{
-    return bookService.Count > 0 ? Results.Ok(bookService.GetBooks()) : throw new BookNotFoundException(0); // Eðer kitap listesi boþ deðilse, kitap listesini döndürür. // Eðer kitap listesi boþsa, hata fýrlatýr.
-})
-    .Produces<List<Book>>(StatusCodes.Status200OK) // Produces, Swagger'da bu endpoint'in baþarýlý durum kodunu ve baþarýlý durum mesajýný gösterir.
-    .Produces(StatusCodes.Status204NoContent) // Eðer kitap listesi boþsa, 204 No Content durum kodunu döndürür.
-    .WithTags("CRUD", "GETs"); // WithTags, Swagger'da bu endpoint'in hangi gruba ait olduðunu gösterir. // Bu endpoint'i "CRUD" ve "GETs" gruplarýna ekler.
+#region Book API Endpoint Extensions
 
-// GET BY ID
-app.MapGet("/api/books/{id}", (int id, IBookService bookService) =>
-{
-    var book = bookService.GetBookById(id); // Kitap listesinden ID'ye göre kitap arar.
-    return Results.Ok(book); // Eðer kitap bulunursa, kitap bilgilerini döndürür. // Eðer kitap bulunamazsa, hata fýrlatýr.
+app.GetAllBooks(); // Bu metotlar, kitaplarý listelemek için kullanýlýr. // Kitap listesini döndürür. // Eðer kitap listesi boþ ise, hata fýrlatýr.
+app.GetBookById(); // Bu metotlar, kitaplarý listelemek ve ID'ye göre kitap aramak için kullanýlýr. // Bu metotlar, API endpoint'lerini haritalar ve gerekli middleware'leri ekler.
+app.PostBook(); // Bu metotlar, yeni kitap eklemek için kullanýlýr. // Kitap ekler ve baþarýlý durum kodunu döndürür. // Eðer kitap doðrulama hatasý varsa, hata mesajýný döndürür.
+app.PutBook(); // Bu metotlar, kitap güncellemek için kullanýlýr. // Kitap günceller ve baþarýlý durum kodunu döndürür. // Eðer kitap bulunamazsa, hata fýrlatýr. // Eðer kitap doðrulama hatasý varsa, hata mesajýný döndürür.
+app.DeleteBook(); // Bu metotlar, kitap silmek için kullanýlýr. // Kitap siler ve baþarýlý durum kodunu döndürür. // Eðer kitap bulunamazsa, hata fýrlatýr.
+app.SearchBooks(); // Bu metotlar, kitaplarý aramak için kullanýlýr. // Kitaplarý arar ve baþarýlý durum kodunu döndürür. // Eðer kitap bulunamazsa, hata fýrlatýr.
 
-    //return Results.Ok(Book.ListWithId(id)); // Book sýnýfýndaki ListWithId metodunu kullanarak ID'ye göre kitap döndürür.
-})
-    .Produces<Book>(StatusCodes.Status200OK) // Produces, Swagger'da bu endpoint'in baþarýlý durum kodunu ve baþarýlý durum mesajýný gösterir.
-    .Produces<ErrorDetails>(StatusCodes.Status404NotFound) // Eðer kitap bulunamazsa, 404 Not Found durum kodunu döndürür.
-    .WithTags("GETs"); // WithTags, Swagger'da bu endpoint'in hangi gruba ait olduðunu gösterir. // Bu endpoint'i "GETs" grubuna ekler.
+#endregion Book API Endpoint Extensions
 
-// POST
-app.MapPost("/api/books", (BookDtoForInsertion InsertBook, IBookService bookService) =>
-{
-    var book = bookService.CreateBook(InsertBook);
-    return Results.Created($"/api/books/{book.Id}", InsertBook); // 201
-})
-    .Produces<Book>(StatusCodes.Status201Created) // Produces, Swagger'da bu endpoint'in baþarýlý durum kodunu ve baþarýlý durum mesajýný gösterir.
-    .Produces<List<ValidationResult>>(StatusCodes.Status422UnprocessableEntity) // Eðer kitap doðrulama hatasý varsa, 422 Unprocessable Entity durum kodunu döndürür.
-    .WithTags("CRUD"); // WithTags, Swagger'da bu endpoint'in hangi gruba ait olduðunu gösterir. // Bu endpoint'i "CRUD" grubuna ekler.
-
-// PUT
-app.MapPut("/api/books/{id}", (int id, BookDtoForUpdate updateBook, IBookService bookService) =>
-{
-    Book book = bookService.UpdateBook(id, updateBook);
-    return Results.Ok(book); // 200
-})
-    .Produces<Book>(StatusCodes.Status200OK)
-    .Produces<ErrorDetails>(StatusCodes.Status404NotFound)
-    .Produces<ErrorDetails>(StatusCodes.Status400BadRequest)
-    .Produces<ErrorDetails>(StatusCodes.Status422UnprocessableEntity)
-    .WithTags("CRUD"); // WithTags, Swagger'da bu endpoint'in hangi gruba ait olduðunu gösterir. // Bu endpoint'i "CRUD" grubuna ekler.
-
-// DELETE
-app.MapDelete("/api/books/{id}", (int id, IBookService bookService) =>
-{
-    bookService.DeleteBook(id); // Kitabý kitap listesinden siler.
-    return Results.NoContent(); // 204
-})
-    .Produces(StatusCodes.Status204NoContent)
-    .Produces<ErrorDetails>(StatusCodes.Status404NotFound)
-    .Produces<ErrorDetails>(StatusCodes.Status400BadRequest)
-    .WithTags("CRUD"); // WithTags, Swagger'da bu endpoint'in hangi gruba ait olduðunu gösterir. // Bu endpoint'i "CRUD" grubuna ekler.
-
-// GET SEARCH
-app.MapGet("/api/books/search", (string? title, IBookService bookService) =>
-{
-    var books = string.IsNullOrEmpty(title) ? bookService.GetBooks() : bookService.GetBooks().Where(x => x.Title is not null && x.Title.Contains(title, StringComparison.OrdinalIgnoreCase)).ToList(); // Eðer title boþ ise, tüm kitaplarý döndürür. // Eðer title dolu ise, kitap listesinden title'a göre arama yapar. // StringComparison.OrdinalIgnoreCase, büyük/küçük harf duyarsýz arama yapar.
-
-    return books.Any() ? Results.Ok(books) : Results.NotFound(); // Eðer kitap listesi boþ deðilse, kitap listesini döndürür. // Eðer kitap listesi boþsa, 404 Not Found durum kodunu döndürür.
-})
-    .Produces<List<Book>>(StatusCodes.Status200OK)
-    .Produces(StatusCodes.Status204NoContent)
-    .Produces<ErrorDetails>(StatusCodes.Status400BadRequest)
-    .WithTags("GETs");
 
 app.UseCors("All"); // Use CORS policy // Use "AllowSpecificOrigin" to restrict to a specific origin
 app.UseCustomExceptionHandler(); // Özel hata iþleyicisini kullanýr. // Bu metot, global hata yakalama middleware'ini ekler. // Hatalarý JSON formatýnda döndürür. // Bu sayede hata mesajlarýný daha okunabilir hale getirir.
